d7d5f2b490764828c2d4fe52eee13728fc1524d5
TobiIchiro <israel.toledo@outlook.es>
2023-11-24 09:26:22 -0600

    Excercises 4-4

--- ShopApp/src/duke/choice/ShopApp.java
+++ ShopApp/src/duke/choice/ShopApp.java
@@ -43,24 +43,34 @@ public class ShopApp {
         
         Clothing item1 = new Clothing();
         Clothing item2 = new Clothing();
-        Clothing[] items = {item1,item2};
+        Clothing[] items = {item1,item2,new Clothing(),new Clothing()};
         
         item1.description = "Blue Jacket";
         item1.price = 20.9;
         item1.size = "M";
+        
         item2.description = "Orange T-Shirt";
         item2.price = 10.5;
         item2.size = "S";
+        
+        items[2].description = "Green Scarf";
+        items[2].price = 5;
+        items[2].size = "S";
+        
+        items[3].description = "Blue T-Shirt";
+        items[3].price = 10.5;
+        items[3].size = "S";
         
         //System.out.println(item1.description + "," +item1.price+","+item1.size);
         //System.out.println(item2.description + "," +item2.price+","+item2.size);
         for(Clothing item : items)
         {
-            System.out.println(item.description + "," +item.price+","+item.size);
-        }
-        for(Clothing item : items)
-        {
-            total += item.price * (1 + tax);
+            if(item.size.equals(c1.size)) 
+            {
+                System.out.println(item.description + "," +item.price+","+item.size);
+                total += item.price * (1 + tax);
+            }
+            if(total > 15)break;
         }
         //total = (2*item2.price + item1.price)*(1+tax);
         System.out.println("Total: " + total);
